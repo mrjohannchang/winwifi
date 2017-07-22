2ade4d6a00467985f2ade9bfe32b691c876914a7
Henry Chang <jrhenry@flotechnologies.com>
2017-07-22 13:28:19 -0700

    Refine setup.py

--- setup.py
+++ setup.py
@@ -56,6 +56,8 @@ setup(
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3.6',
+
+        'Environment :: Win32 (MS Windows)',
     ],
 
     #  # What does your project relate to?
