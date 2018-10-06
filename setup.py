7bf2377a0f40905a10e0dc8ca85350cef66a2393
Johann Chang <mr.changyuheng@gmail.com>
2018-10-06 10:52:28 +0800

    Switch to Mozilla Public License 2.0 (MPL-2.0)

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     author_email='mr.changyuheng@gmail.com',
 
     # Choose your license
-    license='MIT',
+    license='MPL-2.0',
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -51,7 +51,7 @@ setup(
         'Topic :: Utilities',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
