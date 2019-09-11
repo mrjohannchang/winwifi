b53d0cb730634da4644d591a7b5f12f5e5a2009e
Johann Chang <mr.changyuheng@gmail.com>
2019-09-11 10:40:05 +0800

    Bump to 1.0.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.18',
+    version='1.0.0',
 
     description='A Windows Wi-Fi CLI',
     long_description=long_description,
