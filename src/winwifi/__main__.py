9ba461e6aa950c0d10375e377f1551c56e2ff64b
Henry Chang <mr.changyuheng@gmail.com>
2019-11-26 10:43:42 +0800

    Configure the encoding of the stdout to UTF-8 forcefully

--- src/winwifi/__main__.py
+++ src/winwifi/__main__.py
@@ -87,6 +87,7 @@ class WifiForget(plumbum.cli.Application):
 def main() -> int:
     if os.name == 'nt':
         os.system('chcp 65001 >nul 2>&1')
+        sys.stdout.reconfigure(encoding='utf8')
 
     return Wifi.run()[1]
 
