2e9c93f5fb8bf7d6b61d051c62ce23a31a0dcdc6
Henry Chang <jrhenry@flotechnologies.com>
2019-11-26 12:01:46 +0800

    Refine UTF-8 output config

--- src/winwifi/__main__.py
+++ src/winwifi/__main__.py
@@ -87,7 +87,10 @@ class WifiForget(plumbum.cli.Application):
 def main() -> int:
     if os.name == 'nt':
         os.system('chcp 65001 >nul 2>&1')
+    if sys.stdout.encoding != 'utf8':
         sys.stdout.reconfigure(encoding='utf8')
+    if sys.stderr.encoding != 'utf8':
+        sys.stderr.reconfigure(encoding='utf8')
 
     return Wifi.run()[1]
 
